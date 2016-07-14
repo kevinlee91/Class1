3f442135237be09c4f8e31c7bf13c44de606e6d7
Kevin Lee <kevinlee91@gmail.com>
2016-07-13 22:16:53 -0700

    KL - added ball and playercontrol scripts

---
+++ New Unity Project/Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody _CachedRB;
+
+    private Vector3 _Move;
+
+    public void Start()
+    {
+        _CachedRB = GetComponent<Rigidbody>();
+        
+    }
+
+    public void Move(Vector3 move)
+    {
+        _Move += move;
+    }
+
+    public void Jump(Vector3 jump)
+    {
+        _CachedRB.AddForce(jump, ForceMode.Impulse);
+    }
+
+    public void Update()
+    {
+
+    }    
+
+public void FixedUpdate()
+    {
+        _CachedRB.AddForce(_Move);
+
+        _Move = Vector3.zero;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+    [Header("Player object values")]
+    public Ball ball;
+    [Tooltip("Speed in units per second")]
+    [Range(1, 144f)]
+    public float speed = 100;
+    [Range(1, 10f)]
+    public float jumpPower = 5;
+
+    private Vector3 jumpForce;
+    private bool isGrounded;
+
+    public void Start()
+    {
+        jumpForce = new Vector3(0, jumpPower, 0);
+    }
+    public void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float v = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        bool j = Input.GetButtonDown("Jump");
+
+        ball.Move(new Vector3(h, 0, v));
+
+        if(j)
+        {
+            ball.Jump(jumpForce);
+            Debug.Log("pressed jump");
+        }
+    }
+}
